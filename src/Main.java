6076320b9cdcaac055e56f7ad31f1107191a2d4e
posusaevkostay@mail.ru <posusaevkostay@mail.ru>
2017-12-30 12:18:01 +0300

    Task25

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите натуральное число");
+        int n = scanner.nextInt();
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+    }
+}
